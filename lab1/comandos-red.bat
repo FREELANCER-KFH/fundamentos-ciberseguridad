aaa08b27293f92cf5af9067393a8a990ecb221ce
Kevin Feliz Henriquez <108124473+FREELANCER-KFH@users.noreply.github.com>
2026-04-07 13:06:46 -0400

    Add files via upload

---
+++ lab1/comandos-red.bat
@@ -0,0 +1,17 @@
+cls
+echo off
+echo bienvenido/a a mi script en bat
+echo realizado por: kevin feliz
+ipconfig
+ipconfig/all
+ipconfig/displaydns
+ipconfig/flushdns
+ipconfig/release
+ipconfig/renew
+ping google.com
+ping facebook.com
+netstat -a
+getmac
+route print
+echo fin del script, gracias por usarme
+echo on
